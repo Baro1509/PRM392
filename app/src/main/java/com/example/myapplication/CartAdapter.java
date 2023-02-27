15a7828ab2bb2785ebfcbda8e9e95ee2bcb21035
Hungvu <vonglaucac2@gmail.com>
2023-02-28 01:01:09 +0700

    Commit to viewCart

---
+++ app/src/main/java/com/example/myapplication/CartAdapter.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ProductViewHolder>{
+
+    //we are storing all the products in a list
+    private List<Product> productList;
+
+    private Context mCtx;
+
+    public CartAdapter(Context mCtx,List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_cart, null);
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_cart,parent,false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        if (product == null){
+            return;
+        }
+
+        holder.img.setImageResource(product.getImage());
+        holder.productName.setText(product.getProductName());
+        holder.productPrice.setText(product.getPrice().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (productList != null){
+            return productList.size();
+        }
+        return 0;
+    }
+
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView img;
+        private TextView productName;
+        private TextView productPrice;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.imageView);
+            productName = itemView.findViewById(R.id.textViewName);
+            productPrice = itemView.findViewById(R.id.price);
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/ViewCart.java
+++ app/src/main/java/com/example/myapplication/ViewCart.java
@@ -1,55 +1,26 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomePage extends AppCompatActivity implements View.OnClickListener {
-
-    private  RecyclerView rcvProduct;
-    private Button btnPhone,btnLaptop,btnFurniture,btnAdd;
-    private GridLayoutManager grid;
+public class ViewCart extends AppCompatActivity {
+    RecyclerView recyclerView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
-
-        btnPhone = findViewById(R.id.phoneBtn);
-        btnLaptop = findViewById(R.id.laptopBtn);
-        btnFurniture = findViewById(R.id.furniBtn);
-        btnAdd = findViewById(R.id.btnAdd);
-        rcvProduct = findViewById(R.id.rcv_product);
-
-        grid = new GridLayoutManager(this, 2);
-        rcvProduct.setLayoutManager(grid);
-        ProductListViewAdapter productListViewAdapter = new ProductListViewAdapter(getProductList());
-        rcvProduct.setAdapter(productListViewAdapter);
-
-        btnPhone.setOnClickListener(this);
-        btnLaptop.setOnClickListener(this);
-        btnFurniture.setOnClickListener(this);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openCart();
-            }
-        });
+        setContentView(R.layout.activity_view_cart);
+        recyclerView = findViewById(R.id.rcv_product);
+        CartAdapter cartAdapter = new CartAdapter(this,getProductList());
+        recyclerView.setAdapter(cartAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-    public void openCart() {
-        Intent intent = new Intent(this,ViewCart.class );
-        startActivity(intent);
-    }
-
     private List<Product> getProductList() {
         List<Product> productList = new ArrayList<>();
         productList.add(new Product(R.drawable.iphone1,"Iphone",1000.0f,Product.TYPE_PHONE));
@@ -78,28 +49,4 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
         return productList;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.phoneBtn:
-                scrollToItem(0);
-                break;
-            case R.id.laptopBtn:
-                scrollToItem(7);
-                break;
-            case R.id.furniBtn:
-                scrollToItem(14);
-                break;
-            case R.id.btnAdd:
-
-                break;
-        }
-    }
-
-    private void scrollToItem(int index) {
-        if (grid == null){
-            return;
-        }
-        grid.scrollToPositionWithOffset(index,0);
-    }
 }
